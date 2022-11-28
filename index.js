f5b0812677c0e7f4761d68c6d7a8ebce44604bec
surendar06 <surendar@pondybiz.com>
2022-11-28 19:04:14 +0530

    create routes for user & auth

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,  () => {
+     console.log("Mongo DB connected")
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users" , userRoute);
+app.use("/api/auth" , authRoute);
 
 app.listen(3000,() => console.log("Listening to port 3000"))
